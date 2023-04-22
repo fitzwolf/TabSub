1c49e19548b619cc05444453fc607f75fa4aee5c
Caleb <calebthomas646@yahoo.com>
2023-04-22 11:45:53 -0400

    added detection for hand and index finger

--- Camera.py
+++ Camera.py
@@ -6,6 +6,7 @@
 import cv2
 import mediapipe as mp
 
+# MediaPipe Hand Detection ReadMe: https://github.com/google/mediapipe/blob/master/docs/solutions/hands.md
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
@@ -18,6 +19,8 @@ mp_hands = mp.solutions.hands
 
 def draw_index_finger(image, hand_landmarker_result):
 	"""Draws the index finger tip location."""
+	if not hand_landmarker_result or not hand_landmarker_result.multi_hand_landmarks:
+		return image
 	image_rows, image_cols, _ = image.shape
 	for hand_landmarks in hand_landmarker_result.multi_hand_landmarks:
 		index_finger_tip = mp_drawing._normalized_to_pixel_coordinates(
@@ -28,6 +31,8 @@ def draw_index_finger(image, hand_landmarker_result):
 	return image
 
 def draw_hand_landmarks(image, hand_landmarker_result):
+	if not hand_landmarker_result or not hand_landmarker_result.multi_hand_landmarks:
+		return image
 	for hand_landmarks in hand_landmarker_result.multi_hand_landmarks:
 		mp_drawing.draw_landmarks(
 			image,
@@ -45,8 +50,7 @@ vid = cv2.VideoCapture(0)
 
 #with HandLandmarker.create_from_options(options) as landmarker:
 with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-	while(True):
-		
+	while(True):	
 		# Capture the video frame
 		# by frame
 		ret, frame = vid.read()
@@ -62,7 +66,7 @@ with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracki
 		cv2.imshow('frame', frame)
 		image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 		results = hands.process(image)
-		cv2.imshow('hand', cv2.cvtColor(draw_hand_landmarks(image.copy(), results, 0), cv2.COLOR_RGB2BGR))
+		cv2.imshow('hand', cv2.cvtColor(draw_hand_landmarks(image.copy(), results), cv2.COLOR_RGB2BGR))
 		cv2.imshow('index finger', cv2.cvtColor(draw_index_finger(image.copy(), results), cv2.COLOR_RGB2BGR))
 		
 		
