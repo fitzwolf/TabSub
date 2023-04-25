f46d1569cf96b6f2b388a66afb5db18fd638cdf7
Brenda Hariyanto <brendah5@illinois.edu>
2023-04-25 13:52:18 -0500

    done

--- process_picture.py
+++ process_picture.py
@@ -6,12 +6,13 @@ import numpy as np
 # of the points clicked on the image 
 count = 2
 arr = []
-top_left =(0,0)
+top_left = (0,0)
 bottom_right=(0,0)
 i = 0 
 
 
 #source: https://www.geeksforgeeks.org/displaying-the-coordinates-of-the-points-clicked-on-the-image-using-python-opencv/
+#Prompts a window and asked the user to click top left and bottom right corner of their first letter.
 def click_event(event, x, y, flags, params):
     
     # checking for left mouse clicks
@@ -40,7 +41,7 @@ def click_event(event, x, y, flags, params):
         img = cv2.circle(img, (x,y), radius=2, color=(0, 0, 255), thickness=-1)
         cv2.imshow('image', img)
     
-    
+#Process the points to see which one is bottom right or top left
 def process_points(arr):
     global bottom_right
     global top_left
@@ -53,26 +54,28 @@ def process_points(arr):
         top_left = temp2
         bottom_right = temp1
 
-def crop_image(tl,br, i, pad):
-    global img_copy
-    # (h,w) = img_copy.shape
-    # print("h:",h)
-    # print("w:",w)
+#crop the image and output it as a png in the cropped_images folder
+def crop_image(tl,br, pad):
+    global img_copy, i 
+    #only y needs padding because width is most likely gonna be the same -> w is the longest, thus add 3 pixel.
     if(tl[1]-pad <= 0):
         pad_y = 0
     else:
         pad_y = tl[1]-pad
     
-    # if(br[0]+pad >= w):
-    #     pad_x = w
-    # else:
-    pad_x = br[0]+pad
-    crop_img = img_copy[pad_y:br[1]+5, tl[0]:pad_x].copy()
+    crop_img = img_copy[pad_y:br[1]+7, tl[0]:br[0]+7].copy()
     cv2.imwrite(f'./cropped_images/image{i}.png', crop_img)
     i+=1
 
+#update the coordinate to get the next letter
+def update_coor(tl,br):
+    new_tl = (br[0], tl[1])
+    new_br = ( (2*br[0] - tl[0])   ,  br[1])
+    return new_tl,new_br
+
+#main
 if __name__=="__main__":
-    img = cv2.imread('handwriting.png', 1)
+    img = cv2.imread('world.jpeg', 1)
     img_copy = img.copy()
     print("Please click on the top-left corner and the bottom-right corner of your first letter.")
     cv2.imshow('image', img)
@@ -80,5 +83,22 @@ if __name__=="__main__":
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     process_points(arr)
-    crop_image(top_left,bottom_right,i,20)
+    loop=4
+    i=0
+    while(True):
+        #crop the image and output to cropped_images
+        crop_image(top_left,bottom_right,35) 
+        
+        # TODO:
+        # Change if statement to be: 
+        # if image (i-1) and block = background:
+            #break
+        if loop==0:
+            break
+        else:
+            top_left,bottom_right = update_coor(top_left,bottom_right)
+            loop-=1
+
+        
+        
 
