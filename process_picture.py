b078788a54de540b413e12988914bec1b106189c
Brenda Hariyanto <brendah5@illinois.edu>
2023-04-25 14:17:17 -0500

    some error handling

--- process_picture.py
+++ process_picture.py
@@ -57,20 +57,35 @@ def process_points(arr):
 #crop the image and output it as a png in the cropped_images folder
 def crop_image(tl,br, pad):
     global img_copy, i 
+    h,w,c = img_copy.shape
+
     #only y needs padding because width is most likely gonna be the same -> w is the longest, thus add 3 pixel.
     if(tl[1]-pad <= 0):
         pad_y = 0
     else:
         pad_y = tl[1]-pad
+    if(br[0]+7 >= w):
+        pad_x = w
+        print("im here")
+    else:
+        pad_x = br[0]+7
     
-    crop_img = img_copy[pad_y:br[1]+7, tl[0]:br[0]+7].copy()
+    crop_img = img_copy[pad_y:br[1]+7, tl[0]:pad_x].copy()
     cv2.imwrite(f'./cropped_images/image{i}.png', crop_img)
     i+=1
 
 #update the coordinate to get the next letter
 def update_coor(tl,br):
+    global img_copy
+    h,w,c = img_copy.shape
+
     new_tl = (br[0], tl[1])
-    new_br = ( (2*br[0] - tl[0])   ,  br[1])
+    new_br = ( (2*br[0] - tl[0]) ,  br[1])
+
+    if(new_br[0] >= w):
+        new_br = (w,br[1])
+    if(new_br[0] - new_tl[0] < (br[0]-tl[0])):
+        return (-1,-1)
     return new_tl,new_br
 
 #main
@@ -83,7 +98,7 @@ if __name__=="__main__":
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     process_points(arr)
-    loop=4
+    loop=6
     i=0
     while(True):
         #crop the image and output to cropped_images
@@ -97,6 +112,10 @@ if __name__=="__main__":
             break
         else:
             top_left,bottom_right = update_coor(top_left,bottom_right)
+            #We are at the end of the picture
+            if(top_left == -1):
+                print("Nothing left to read")
+                break
             loop-=1
 
         
